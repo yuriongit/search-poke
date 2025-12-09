6426530cf6f485e5537f835902e463d6cc1805e9
yuriongit <produceryurii@gmail.com>
2025-12-09 13:45:19 -0500

    added enter keypress on search input

--- script.js
+++ script.js
@@ -4,10 +4,15 @@ const buttonEl = document.getElementById('pokeButton');
 const pokeImg = document.getElementById("pokeImg");
 const pokeImg2 = document.getElementById("pokeImg2");
 const pokePTag = document.getElementById("pokeP");
+
 const pokeUlTag = document.getElementById("pokePhys");
+const pokeUlTag3 = document.getElementById("pokeMovesAndAbilities");
 const pokeUlTag2 = document.getElementById("pokeOther");
+
 const pokePhysLabel = document.getElementById("pokePhysLabel");
 const pokeOtherLabel = document.getElementById("pokeOtherLabel");
+const pokeMovesAndAbilitesLabel = document.getElementById("pokeMovesAndAbilitesLabel");
+
 const validPokemonCharacters = [
     "pikachu",
     "charizard",
@@ -19,9 +24,15 @@ const validPokemonCharacters = [
     "ditto",
     "blastoise",
     "jigglypuff",
-    "snorlax",
+    "snorlax",'umbreon',
+    'mimikyu',
+    'dragapult',
+    'dragonite',
+    'mudkip',
+    'garchomp'
 ];
 
+const pageNum = document.getElementById("pageNum");
 
 const fetchData = async (pokemonCharacter) => {
     try {
@@ -32,6 +43,11 @@ const fetchData = async (pokemonCharacter) => {
         const data = await response.json();
         const sprite_front = data.sprites.other.showdown.front_default;
         const sprite_back = data.sprites.other.showdown.back_default;
+
+        pokeUlTag.textContent = "";
+        pokeUlTag2.textContent = "";
+
+        pageNum.textContent = "1"
 
         const pokeInfo = [
             {
@@ -45,14 +61,6 @@ const fetchData = async (pokemonCharacter) => {
                         stat: `${data.height} ft`,
                     },
                     {
-                        label: "ability",
-                        stat: data.abilities.map((i) => " " + i.ability.name),
-                    },
-                    {
-                        label: "moves",
-                        stat: data.moves.map((i) => " " + i.move.name),
-                    },
-                    {
                         label: "forms",
                         stat: data.forms.map((i) => " " + i.name),
                     },
@@ -64,6 +72,17 @@ const fetchData = async (pokemonCharacter) => {
                         label: "type name",
                         stat: data.types.map((i) => " " + i.type.name),
                     },
+                ],
+                movesAndAbilities: [
+                    {
+                        label: "ability",
+                        stat: data.abilities.map((i) => " " + i.ability.name),
+                    },
+                    {
+                        label: "moves",
+                        stat: data.moves.map((i) => " " + i.move.name),
+                    },
+
                 ],
                 OtherInfo: [
                     {
@@ -81,9 +100,14 @@ const fetchData = async (pokemonCharacter) => {
                 ],
             },
         ];
-        
         pokeInfo.map(i => {
-            i.physicalStats.map(i => {
+
+            pokePhysLabel.textContent = `Physical Stats`;
+            pokeOtherLabel.textContent = `Other Info`;
+            pokeMovesAndAbilitesLabel.textContent = `Moves & Abilities`;
+
+            pokeUlTag.appendChild(pokePhysLabel);
+            i.physicalStats.map((i) => {
                 const li = document.createElement("li");
                 li.classList.add("text-zinc-300/85");
                 li.classList.add("overflow-none");
@@ -95,26 +119,59 @@ const fetchData = async (pokemonCharacter) => {
                 li.classList.add(`[&::-webkit-scrollbar-thumb]:bg-purple-500`);
                 li.textContent = `${i.label}: ${i.stat}`;
                 pokeUlTag.appendChild(li);
-            })
-            i.OtherInfo.map((i) => {
+            });
+            pokeUlTag2.appendChild(pokeMovesAndAbilitesLabel);
+            i.movesAndAbilities.map((i) => {
                 const li2 = document.createElement("li");
                 li2.classList.add("text-zinc-300/85");
                 li2.classList.add("overflow-none");
                 li2.classList.add("overflow-y-auto");
-                li2.classList.add("max-h-14");
+                li2.classList.add("max-h-20");
                 li2.classList.add("text-zinc-300/85");
-                li2.classList.add("[&::-webkit-scrollbar-track]:rounded-full");
-                li2.classList.add("[&::-webkit-scrollbar]:w-1");
-                li2.classList.add("[&::-webkit-scrollbar-thumb]:bg-purple-600");
+                li2.classList.add(`[&::-webkit-scrollbar-track]:rounded-full`);
+                li2.classList.add(`[&::-webkit-scrollbar]:w-2`);
+                li2.classList.add(`[&::-webkit-scrollbar-thumb]:bg-purple-500`);
                 li2.textContent = `${i.label}: ${i.stat}`;
                 pokeUlTag2.appendChild(li2);
             });
-        })
+            pokeUlTag3.appendChild(pokeOtherLabel);
+            i.OtherInfo.map((i) => {
+                const li3 = document.createElement("li");
+                li3.classList.add("text-zinc-300/85");
+                li3.classList.add("overflow-none");
+                li3.classList.add("overflow-y-auto");
+                li3.classList.add("max-h-14");
+                li3.classList.add("text-zinc-300/85");
+                li3.classList.add("[&::-webkit-scrollbar-track]:rounded-full");
+                li3.classList.add("[&::-webkit-scrollbar]:w-1");
+                li3.classList.add("[&::-webkit-scrollbar-thumb]:bg-purple-600");
+                li3.textContent = `${i.label}: ${i.stat}`;
+                pokeUlTag3.appendChild(li3);
+            });
+        });
         
-        ;
-        
+
         pokeImg.src = sprite_front;
         pokeImg2.src = sprite_back;
+
+
+        const prevImgBtn = document.getElementById("prevImg");
+        const nextImgBtn = document.getElementById("nextImg");
+        prevImgBtn.addEventListener("click", () => {
+            pokeImg.src = sprite_front;
+            pokeImg2.src = sprite_back;
+            pageNum.textContent = "1";
+            
+        });
+        
+        nextImgBtn.addEventListener("click", () => {
+            const sprite_frontAlt = data.sprites.other.showdown.front_shiny;
+            const sprite_backAlt = data.sprites.other.showdown.back_shiny;
+
+            pokeImg.src = sprite_frontAlt
+            pokeImg2.src = sprite_backAlt
+            pageNum.textContent = '2'
+        });
         
         inputEl.textContent = '';
     } catch (error) {
@@ -128,11 +185,23 @@ buttonEl.addEventListener('click', () => {
     
     if (validPokemonCharacters.includes(pokemonCharacter.toLowerCase())) {
         pokePTag.textContent = `${pokemonCharacter.toUpperCase()}`;
-        pokePhysLabel.textContent = `Physical Stats`;
-        pokeOtherLabel.textContent = `Other Info`;
         inputEl.value = ""
         fetchData(pokemonCharacter);
     } else {
-        return
+        null
     }
 })
+
+inputEl.addEventListener("keydown", (e) => {
+    if(e.key === 'Enter'){
+        const pokemonCharacter = inputEl.value;
+
+        if (validPokemonCharacters.includes(pokemonCharacter.toLowerCase())) {
+            pokePTag.textContent = `${pokemonCharacter.toUpperCase()}`;
+            inputEl.value = "";
+            fetchData(pokemonCharacter);
+        } else {
+            return;
+        }
+    }
+});
