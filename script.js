6e6d3537e994c85aa35f8a51886002d5cae5a9b9
yuriongit <produceryurii@gmail.com>
2025-12-12 20:02:28 -0500

    reworked layout and added poke-cries audios, added formatting to data, new info sections added.

--- script.js
+++ script.js
@@ -5,14 +5,18 @@ const pokeImg2 = document.getElementById("pokeImg2");
 const pokePTag = document.getElementById("pokeP");
 
 const pokePhysLabel = document.getElementById("pokePhysLabel");
+
+
 const pokeOtherLabel = document.getElementById("pokeOtherLabel");
 const pokeMovesAndAbilitesLabel = document.getElementById("pokeMovesAndAbilitesLabel");
 
 let detailsDiv = document.getElementById("purplePreviewDiv");
 
+// IMAGES DIV AND ELEMENTS
 const imagesBox = document.getElementById("imagesBox");
 const prevImgBtn = document.getElementById("prevImg");
 const nextImgBtn = document.getElementById("nextImg");
+// IMAGES DIV AND ELEMENTS
 
 // PAGE-NUM
 const pageNum = document.getElementById("pageNum");
@@ -22,9 +26,53 @@ const popularPokemonsBtnsDiv = document.getElementById(
     "popularPokemonsBtnsDiv"
 );
 
+// LIST FORMATTER
+const listFormatter = new Intl.ListFormat("en", {
+    style: "long",
+    type: "conjunction",
+});
+
+const numFormatter = new Intl.NumberFormat("en-US");
+// LIST FORMATTER
+
 // VALID POKEMONS
 const validPokemonCharacters = [
     "pikachu",
+    "pikachu-pop-star",
+    "ivysaur",
+    "venusaur",
+    "caterpie",
+    "metapod",
+    "butterfree",
+    "weedle",
+    "kakuna",
+    "beedrill",
+    "pidgey",
+    "pidgeotto",
+    "pidgeot",
+    "rattata",
+    "raticate",
+    "spearow",
+    "fearow",
+    "ekans",
+    "arbok",
+    "raichu",
+    "sandshrew",
+    "sandslash",
+    "nidoran-f",
+    "nidorina",
+    "nidoqueen",
+    "nidoran-m",
+    "nidorino",
+    "nidoking",
+    "clefairy",
+    "cleffa",
+    "clefable",
+    "vulpix",
+    "wigglytuff",
+    "igglybuff",
+
+    "pichu",
     "charizard",
     "squirtle",
     "charmander",
@@ -35,28 +83,42 @@ const validPokemonCharacters = [
     "blastoise",
     "jigglypuff",
     "snorlax",
+    "munchlax",
     "umbreon",
     "dragapult",
     "dragonite",
+    "dragonair",
     "mudkip",
     "garchomp",
     "arcanine",
+    "growlithe",
     "lugia",
-    "lucario",
-    "greninja",
     "wartortle",
     "poliwrath",
     "lickitung",
     "rayquaza",
     "haunter",
+    "drakloak",
+    "dreepy",
     "meowth",
     "tyranitar",
+    "gastly",
+    "pupitar",
+    "larvitar",
+    "charmeleon",
 ];
 // VALID POKEMONS
 
 // STYLINGS
 const wrapperDivStyles = ["uppercase", "h-fit"];
-const headerStyles = ["text-2xl", "font-semibold"];
+const headerStyles = [
+    "text-2xl",
+    "font-bold",
+    "underline",
+    "decoration-1",
+    "decoration-purple-500",
+    "underline-offset-4",
+];
 const ulStyles = ["flex", "flex-col", "text-wrap", "gap-4", "max-w-md"];
 const liStyles = [
     "[&::-webkit-scrollbar-thumb]:bg-purple-600",
@@ -74,11 +136,27 @@ const liStyles = [
 const liSpanStyles = ["text-purple-600", "font-light"];
 // STYLINGS
 
-// API CALL
-const reqData = async (pokemonCharacter) => {
+// API CALL 1
+const reqData1 = async (pokemonCharacter) => {
     try {
         const response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${pokemonCharacter}`
+        );
+        if (!response.ok) {
+            throw new Error(`Could not fetch ${pokemonCharacter}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.log(error);
+    }
+};
+// API CALL 1
+
+// API CALL 2
+const reqData2 = async (pokemonCharacter) => {
+    try {
+        const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon-species/${pokemonCharacter}`
         );
         if (!response.ok) {
             throw new Error("Could not fetch resource");
@@ -88,7 +166,7 @@ const reqData = async (pokemonCharacter) => {
         inputSearch.value = error;
     }
 };
-// API CALL
+// API CALL 2
 
 // POPULAR POKEMONS ARRAY
 const mostPopularPokes = [
@@ -102,7 +180,7 @@ const mostPopularPokes = [
 // POPULAR POKEMONS ARRAY
 
 const previewPokemon = async () => {
-    const data = await reqData("gengar");
+    const data = await reqData1(mostPopularPokes[Math.floor(Math.random() * mostPopularPokes.length)]);
     pokeImg.src = data.sprites.other.showdown.front_default;
     pokeImg2.src = data.sprites.other.showdown.back_default;
 };
@@ -115,16 +193,10 @@ prevImgBtn.classList.add("hover:cursor-not-allowed");
 nextImgBtn.classList.add("hover:cursor-not-allowed");
 // MYSTERY POKEMON INITIAL LOAD
 
-
 // EXECUTE POKEMON SEARCH
 const searchPokemon = async () => {
     const pokemonCharacter = inputSearch.value;
     inputSearch.textContent = "";
-
-    pokeImg.classList.remove("brightness-0");
-    pokeImg2.classList.remove("brightness-0");
-    prevImgBtn.classList.remove("hover:cursor-not-allowed");
-    nextImgBtn.classList.remove("hover:cursor-not-allowed");
 
     // IF POKEMON SUBMITED IN VALIDPOKEMONS RUN SEARCH
     if (validPokemonCharacters.includes(pokemonCharacter.toLowerCase())) {
@@ -133,9 +205,10 @@ const searchPokemon = async () => {
         pageNum.textContent = "1";
         pokePTag.textContent = `${pokemonCharacter.toUpperCase()}`;
         inputSearch.value = "";
-        reqData(pokemonCharacter);
+        reqData1(pokemonCharacter);
 
-        const data = await reqData(pokemonCharacter);
+        const data = await reqData1(pokemonCharacter);
+        const data2 = await reqData2(pokemonCharacter)
 
         detailsDiv.textContent = "";
         // IF POKEMON SUBMITED IN VALIDPOKEMONS RUN SEARCH
@@ -144,6 +217,12 @@ const searchPokemon = async () => {
         const pokeDetails = (obj) => {
             const wrapperDiv = document.createElement("div");
             const pokeUl = document.createElement("ul");
+
+            const pokeLatestCryAudio = document.createElement('audio');
+            const pokeLatestCryPlayBtn = document.createElement("button");
+
+            const pokeLegacyCryAudio = document.createElement("audio");
+            const pokeLegacyCryPlayBtn = document.createElement("button");
 
             obj.map((i) => {
                 const header = document.createElement("label");
@@ -154,6 +233,43 @@ const searchPokemon = async () => {
                 pokeLi.textContent = i.label;
                 pokeLiSpan.textContent = i.stat;
 
+                if(i.label == 'cries: '){
+                    const liStylesForBtns = ["items-center", 'flex', 'gap-2.5']
+                    const playBtnsStyles = [
+                        "bx",
+                        "bx-play",
+                        "text-xl",
+                        "border",
+                        "rounded-full",
+                        "p-1",
+                        "cursor-pointer",
+                        "hover:bg-white/5",
+                        "transition-all",
+                        "border-zinc-800",
+                        "text-purple-600",
+                    ];
+
+                    const hasCry = (info, playBtn) => {
+                        if(info.textContent == "") {
+                            playBtn.classList.add("hidden");
+                        }
+                    }
+                    hasCry(i.stat, pokeLatestCryPlayBtn);
+                    hasCry(i.stat2, pokeLegacyCryPlayBtn);  
+                        
+                    pokeLatestCryPlayBtn.classList.add(...playBtnsStyles);
+                    pokeLegacyCryPlayBtn.classList.add(...playBtnsStyles);
+
+                    pokeLi.classList.add(...liStylesForBtns);
+
+                    pokeLi.appendChild(pokeLatestCryPlayBtn);
+                    pokeLi.appendChild(pokeLegacyCryPlayBtn);
+                    pokeLiSpan.classList.add('hidden');
+
+                    pokeLatestCryAudio.src = i.stat;
+                    pokeLegacyCryAudio.src = i.stat2;
+                }
+                
                 wrapperDiv.classList.add(...wrapperDivStyles);
                 header.classList.add(...headerStyles);
                 pokeUl.classList.add(...ulStyles);
@@ -166,6 +282,19 @@ const searchPokemon = async () => {
                 wrapperDiv.appendChild(pokeUl);
                 detailsDiv.appendChild(wrapperDiv);
             });
+            const audioPlayer = (audio, audioBtn) => {
+                audioBtn.addEventListener("click", () => {
+                    audio.play();
+                    audioBtn.classList.remove("bx-play");
+                    audioBtn.classList.add("bx-pause");
+                });
+                audio.addEventListener("ended", () => {
+                    audioBtn.classList.remove("bx-pause");
+                    audioBtn.classList.add("bx-play");
+                });
+            }
+            audioPlayer(pokeLatestCryAudio, pokeLatestCryPlayBtn)
+            audioPlayer(pokeLegacyCryAudio, pokeLegacyCryPlayBtn)
         };
         // POKEMON DETAILS UL
 
@@ -177,23 +306,33 @@ const searchPokemon = async () => {
                 },
                 {
                     label: "height: ",
-                    stat: `${data.height} ft`,
+                    stat: `${data.height}' ft | ${(data.height * 30.48).toFixed(
+                        2
+                    )} cm`,
                 },
                 {
                     label: "weight: ",
-                    stat: `${data.weight} lbs`,
+                    stat: `${numFormatter.format(data.weight)} lbs | ${numFormatter.format((data.weight / 2.2).toFixed(2))} kg`,
                 },
                 {
                     label: "forms: ",
-                    stat: data.forms.map((i) => " " + i.name),
+                    stat: data.forms.map((i) => i.name).join(", "),
                 },
                 {
-                    label: "type slot: ",
-                    stat: data.types.map((i) => " " + i.slot),
+                    label: "forms switchable: ",
+                    stat: data2.forms_switchable,
                 },
                 {
-                    label: "type name: ",
-                    stat: data.types.map((i) => " " + i.type.name),
+                    label: "type: ",
+                    stat: listFormatter.format(data.types.map((i) => i.type.name)),
+                },
+                {
+                    label: "growth rate: ",
+                    stat: data2.growth_rate.name,
+                },
+                {
+                    label: "shape: ",
+                    stat: data2.shape.name,
                 },
             ],
             movesAndAbilities: [
@@ -202,29 +341,90 @@ const searchPokemon = async () => {
                 },
                 {
                     label: "moves: ",
-                    stat: data.moves.map((i) => " " + i.move.name),
+                    stat: listFormatter.format(data.moves.map(i => i.move.name)),
+                    // listFormatter.format(data.moves.map((i) => i.move.name))
                 },
                 {
                     label: "ability: ",
-                    stat: data.abilities.map((i) => " " + i.ability.name),
+                    stat: listFormatter.format(data.abilities.map((i) => i.ability.name))
                 },
             ],
-            OtherInfo: [
+            species: [
+                {
+                    header: "species",
+                },
+                {
+                    label: "egg group: ",
+                    stat:
+                        listFormatter.format(
+                            data2.egg_groups.map((i) => i.name)
+                        ) || "N/A",
+                },
+                {
+                    label: "habitat: ",
+                    stat: data2.habitat?.name || "N/A",
+                },
+                {
+                    label: "capture rate: ",
+                    stat: `${data2.capture_rate}/255`,
+                },
+            ],
+            history: [
+                {
+                    header: "history",
+                },
+                {
+                    label: "evolves from: ",
+                    stat: data2.evolves_from_species?.name || "N/A",
+                },
+                {
+                    label: "generation: ",
+                    stat: data2.generation.name,
+                },
+            ],
+            otherInfo: [
                 {
                     header: "other info",
                 },
                 {
                     label: "base experience: ",
-                    stat: data.base_experience + " exp",
+                    stat: `${data.base_experience} exp`,
+                },
+                {
+                    label: "base happiness: ",
+                    stat: `${data2.base_happiness} / 255`,
                 },
                 {
                     label: "base stat: ",
-                    stat: data.stats.map((i) => " " + i.base_stat),
+                    stat: data.stats.map((i) => i.base_stat).join(", "),
                 },
                 {
                     label: "stat type: ",
-                    stat: data.stats.map((i) => " " + i.effort),
+                    stat: data.stats.map((i) => i.effort).join(", "),
                 },
+                {
+                    label: "gender rate: ",
+                    stat: data2.gender_rate,
+                },
+            ],
+            itemsAndMisc: [
+                {
+                    header: "held items & misc",
+                },
+                {
+                    label: "held items: ",
+                    stat:
+                        data.held_items.length > 0
+                            ? listFormatter.format(
+                                  data.held_items.map((i) => i.item.name)
+                              )
+                            : "None",
+                },
+                {
+                    label: 'cries: ',
+                    stat: data.cries.latest,
+                    stat2: data.cries.legacy,
+                }
             ],
         };
         // POKEMON INFO ARRAY
@@ -242,6 +442,11 @@ const searchPokemon = async () => {
         const spriteBackAlt = data.sprites.other.showdown.back_shiny;
         // POKEMON IMAGES
 
+        pokeImg.classList.remove("brightness-0");
+        pokeImg2.classList.remove("brightness-0");
+        prevImgBtn.classList.remove("hover:cursor-not-allowed");
+        nextImgBtn.classList.remove("hover:cursor-not-allowed");
+
         // POKEMON IMAGES SET
         pokeImgSetter(pokeImg, spriteFront);
         pokeImgSetter(pokeImg2, spriteBack);
@@ -253,7 +458,10 @@ const searchPokemon = async () => {
         // CREATE POKEMON DETAILS
         pokeDetails(pokeInfo.physicalStats);
         pokeDetails(pokeInfo.movesAndAbilities);
-        pokeDetails(pokeInfo.OtherInfo);
+        pokeDetails(pokeInfo.otherInfo);
+        pokeDetails(pokeInfo.history);
+        pokeDetails(pokeInfo.species);
+        pokeDetails(pokeInfo.itemsAndMisc);
         // CREATE POKEMON DETAILS
 
         // PAGE-SWITCHERS BUTTON EVENT LISTENERS
@@ -309,20 +517,18 @@ popularPokemons();
 // POPULAR POKEMONS BUTTONS AND SEARCH EXECUTION
 
 // EVENT LISTENERS - CLICK, ENTER, PREV + NEXT IMG
-searchBtn.addEventListener("click", searchPokemon);
-inputSearch.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-        searchPokemon();
-        imagesBox.classList.add(...["flex-row-reverse", "gap-7.5"]);
-        if (pokePTag != inputSearch) {
-            pageNum.textContent = "1";
-            inputSearch.value = "";
-        } else {
-            return;
-        }
-    } else {
+searchBtn.addEventListener("click", () => {
+    if (inputSearch.value == "") { 
         return;
+    } searchPokemon();
+});
+inputSearch.addEventListener("keydown", (e) => {
+    if (inputSearch.value == "") {
+        return;
+    } if (e.key === "Enter") {
+        searchPokemon();
     }
+    
 });
 // EVENT LISTENERS - CLICK, ENTER, PREV + NEXT IMG
 
